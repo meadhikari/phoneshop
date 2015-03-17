5f47255326fb65cfcb38a4e4147aaa2c93753067
Bikram Adhikari <salik.adhikari@gmail.com>
2015-03-18 00:50:02 +0545

    angebot auto update

--- www/js/page/kauf.js
+++ www/js/page/kauf.js
@@ -249,7 +249,15 @@ function getBase64Image(img) {
         }); 
         populateSelectPhones(ARTICLES)
     
-}); 
+});
+$.get("http://s250217848.online.de/api/public/index.php/article/all?token="+token, function (data) {
+              $("#offer_tab").empty();
+                  articles = data.article
+                  for (var i = 0; i <articles.length; i++) {
+                      val = "<div class='row offer-row'><div class='col-xs-12 col-sm-12'><p>"+articles[i].manufacturer+"<span>"+articles[i]["name"]+"</span><span>"+articles[i].imei+"</span></p></div></div>"
+                            $("#offer_tab").append(val);
+                  }
+            }); 
     }
   }
 
--- www/js/page/offer.js
+++ www/js/page/offer.js
@@ -1,10 +1,9 @@
 $.get("http://s250217848.online.de/api/public/index.php/article/all?token="+token, function (data) {
+		$("#offer_tab").empty();
         articles = data.article
         for (var i = 0; i <articles.length; i++) {
             val = "<div class='row offer-row'><div class='col-xs-12 col-sm-12'><p>"+articles[i].manufacturer+"<span>"+articles[i]["name"]+"</span><span>"+articles[i].imei+"</span></p></div></div>"
             //console.log(articles[i])
             $("#offer_tab").append(val);
-          
-          
         }
-      });
+});
